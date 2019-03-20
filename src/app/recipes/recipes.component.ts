02aa1e0e33483a88cf8b8fac0eec7f1219c012a5
Bludwarf <bludwarf@gmail.com>
2019-03-20 14:32:26 +0100

    Craft Tools filter

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {DynamicDataSource, DynamicFlatNode, DynamicFlatTreeControl} from '../common/dynamic-flat-tree';
 import {RecipeItem, RecipesDatabase} from './recipes.database';
 import {LocalizationService} from '../services/config/localization.service';
@@ -14,6 +14,18 @@ export const CRAFT_AREA_ICONS = {
   workbench: 'assets/UIAtlasItemIcons/ItemIcons/ui_game_symbol_workbench.png'
 };
 
+export const CRAFT_TOOLS_CAMPFIRE = [
+  'toolCookingPot',
+  'toolCookingGrill',
+  'toolBeaker'
+];
+
+export const CRAFT_TOOLS_FORGE = [
+  'toolAnvil',
+  'toolForgeCrucible',
+  'toolAndDieSet'
+];
+
 // Angular Material does not filter when filter is empty
 const FORCE_FILTER = '<force_filter>';
 
@@ -28,10 +40,9 @@ export class RecipesComponent implements OnInit {
   dataSource: DynamicDataSource<RecipeItem>;
   @ViewChild(SummaryComponent) summary: SummaryComponent;
 
-  @ViewChild(MatButtonToggleGroup)
-  craftAreaFilter: MatButtonToggleGroup;
-  CRAFT_AREA_ICONS = CRAFT_AREA_ICONS;
+  @ViewChildren(MatButtonToggleGroup) toggleGroups: QueryList<MatButtonToggleGroup>;
   CRAFT_AREAS = Object.keys(CRAFT_AREA_ICONS);
+  CRAFT_TOOLS = CRAFT_TOOLS_CAMPFIRE.concat(CRAFT_TOOLS_FORGE);
 
   constructor(database: RecipesDatabase, private localization: LocalizationService, public perks: PerksService) {
     this.treeControl = new DynamicFlatTreeControl<RecipeItem>();
@@ -39,19 +50,31 @@ export class RecipesComponent implements OnInit {
     this.dataSource.data = database.initialData();
     this.dataSource.filterChange.subscribe(value => console.log('filterChange', value));
     this.dataSource.filterPredicate = (recipeItem: RecipeItem, filter) => {
-      // Name filter
-      const nameFiltered = filter === FORCE_FILTER
-        || recipeItem.item.name.toLowerCase().indexOf(filter) !== -1 // Without translation
-        || this.localization.translate(recipeItem.item.name).toLowerCase().indexOf(filter) !== -1;
-      return nameFiltered
-        && (!recipeItem.recipe.craftArea || this.craftAreaFilter.value.includes(recipeItem.recipe.craftArea))
-      ;
+      return this.filterByName(recipeItem, filter) && this.filterByCraftArea(recipeItem) && this.filterByCraftTool(recipeItem);
     };
   }
+
+  get craftAreaFilter(): MatButtonToggleGroup {
+    return this.toggleGroups.first;
+  }
+  get craftToolFilter(): MatButtonToggleGroup {
+    return this.toggleGroups.last;
+  }
+
+  filterByName = (recipeItem: RecipeItem, filter) => filter === FORCE_FILTER
+    || recipeItem.item.name.toLowerCase().indexOf(filter) !== -1 // Without translation
+    || this.localization.translate(recipeItem.item.name).toLowerCase().indexOf(filter) !== -1;
+
+  filterByCraftArea = (recipeItem: RecipeItem) => !recipeItem.recipe.craftArea
+    || this.craftAreaFilter.value.includes(recipeItem.recipe.craftArea);
+
+  filterByCraftTool = (recipeItem: RecipeItem) => !recipeItem.recipe.craftTool
+    || this.craftToolFilter.value.includes(recipeItem.recipe.craftTool);
 
   hasChildren = (_: number, nodeData: DynamicFlatNode<RecipeItem>) => nodeData.hasChildren;
 
   ngOnInit(): void {
+
     this.dataSource.itemExpanded.subscribe(item => item && this.summary.remove(item));
     this.dataSource.itemCollapsed.subscribe(item => item && this.summary.push(item));
     this.dataSource.itemsAppeared.subscribe(nodes => nodes.forEach(node => this.summary.push(node)));
@@ -80,5 +103,10 @@ export class RecipesComponent implements OnInit {
 
   getCraftAreaIcon(craftArea: string) {
     return CRAFT_AREA_ICONS[craftArea];
+  }
+
+  toggleCraftArea(selectedCraftAreas: string[]): void {
+    this.CRAFT_TOOLS = (selectedCraftAreas.includes('campfire') ? CRAFT_TOOLS_CAMPFIRE : [])
+      .concat(selectedCraftAreas.includes('forge') ? CRAFT_TOOLS_FORGE : []);
   }
 }
