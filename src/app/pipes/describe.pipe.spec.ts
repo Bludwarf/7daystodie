330cc33aa6a4c09e6da526eb1f07c46f73429939
Bludwarf <bludwarf@gmail.com>
2019-05-11 11:05:39 +0200

    TU fix

--- src/app/pipes/describe.pipe.spec.ts
+++ src/app/pipes/describe.pipe.spec.ts
@@ -1,12 +1,13 @@
 import {DescribePipe} from './describe.pipe';
 import {TestBed} from '@angular/core/testing';
 import {LocalizationService} from '../localization/localization.service';
+import {DomSanitizer} from '@angular/platform-browser';
 
 describe('DescribePipe', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('create an instance', () => {
-    const pipe = new DescribePipe(TestBed.get(LocalizationService));
+    const pipe = new DescribePipe(TestBed.get(LocalizationService, DomSanitizer));
     expect(pipe).toBeTruthy();
   });
 });
