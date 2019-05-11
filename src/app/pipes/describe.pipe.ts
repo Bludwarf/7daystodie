404b6ea25f889dd23f67feb2fb089ef1afc60992
Bludwarf <bludwarf@gmail.com>
2019-05-11 10:46:11 +0200

    describe with html entities

--- src/app/pipes/describe.pipe.ts
+++ src/app/pipes/describe.pipe.ts
@@ -1,15 +1,37 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import {LocalizationService} from '../localization/localization.service';
+import {DomSanitizer} from '@angular/platform-browser';
 
+/**
+ * Exemple d'utilisation :
+ * <pre>
+ * <p [innerHTML]="object.name | describe"></p>
+ * </pre>
+ *
+ * <p>Attention à ne pas utiliser cette version si la description contient des caractères HTML :</p>
+ *
+ * <pre>
+ * <p>
+ *     {{object.name | describe}}
+ * </p>
+ * </pre>
+ *
+ * <p>Source : <a href="https://stackoverflow.com/a/45352089/1655155">https://stackoverflow.com/a/45352089/1655155</a></p>
+ *
+ */
 @Pipe({
   name: 'describe'
 })
 export class DescribePipe implements PipeTransform {
 
-  constructor(private localization: LocalizationService) { }
+  constructor(private localization: LocalizationService, private sanitizer: DomSanitizer) { }
 
   transform(value: string, lang?: string): any {
-    return this.localization.describe(value);
+    const description = this.localization.describe(value);
+    if (!description) {
+      return description;
+    }
+    return this.sanitizer.bypassSecurityTrustHtml(description.replace('\\n', '<br/>'));
   }
 
 }
